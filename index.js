b862883b7aec7ca8f490da9668852f65f8068ff1
Luciano Franca <lucianopf@outlook.com>
2018-11-07 19:03:54 -0200

    parse: proxy now works for xmls

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const fetch = require('./lib/request')
 const app = express()
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+function sanitizeHeaders(headers) {
+  let sanitizedHeaders = {
+    ...headers,
+    origin: null,
+    host: null,
+    referer: null,
+    connection: null,
+    gzip: false,
+    'content-length': null,
+    'access-control-request-method': null,
+    'access-control-request-headers': null,
+  }
+  return Object
+    .keys(sanitizedHeaders)
+    .reduce((result, headerKey) => {
+      if (sanitizedHeaders[headerKey]) {
+        result[headerKey] = sanitizedHeaders[headerKey]
+      }
+      return result
+    }, {})
+}
 
+app.use(cors())
+app.use(bodyParser.text({type: '*/*'}))
 app.use('/favicon.ico', express.static('assets/favicon.ico'))
 
 app.get('/', (req, res) => {
@@ -16,15 +36,23 @@ app.get('/', (req, res) => {
 
 app.use('/:url', (req, res) => {
   const reqUrl = req.originalUrl.split('').splice(1).join('')
-  console.log('Request made to: ', reqUrl)
-  console.log('Headers', req.headers)
+  const sanitizedHeaders = sanitizeHeaders(req.headers)
   const options = {
     url: reqUrl,
     method: req.method,
-    headers: Object.assign({}, req.headers, { Origin: null, Referer: null })
+    headers: sanitizedHeaders
   }
+
+  if (req.body.length) {
+    options.body = req.body.replace(/\n/g, '')
+  } else if (Object.keys(req.body).length) {
+    options.body = JSON.stringify(req.body)
+  }
+
+  console.log('Request made to:', reqUrl)
   return fetch(reqUrl, options)
-    .then(({ response, body}) => {
+    .then(({ response, body }) => {
+      res.header('content-type', response.headers['content-type'])
       res.send(body)
     })
     .catch(err => {
